95834201ec72a6e87eed88080a4d3f48173d542d
Nhan Vo <niick7@gmail.com>
2024-11-28 23:41:42 -0600

    Create an effect for a signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,4 +11,24 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'angular-signals-example';
   theme = signal('light');
+  label = this.theme();
+
+  constructor() {
+    // Effect work as a listener
+    effect(()=> {
+      // Whenever we change a theme signal anywhere in the code
+      // This effect will run
+      // Why? Because we are using the value of the signal here: this.label = this.theme();
+      // In the other words, effect depends on the theme signal
+      this.label = this.theme();
+    })
+    // The effect will at least run once
+  }
+
+  toggleDarkmode() {
+    // this.theme.set(this.theme() === 'light' ? 'dark' : 'light');
+    this.theme.update((currentValue) =>
+      currentValue === 'light' ? 'dark' : 'light'
+    );
+  }
 }
